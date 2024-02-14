b10e32eade202416de85a583e9cc97b942f8f432
VE3SVF <VE3SVF@gmail.com>
2024-02-14 17:49:30 -0500

    Stuff

--- sondehub_config.py
+++ sondehub_config.py
@@ -1,10 +1,10 @@
-CALLSIGN = "VE3SVF_TESTING"
+CALLSIGN = "TESTING"
 LAT = 43.6689
 LONG = -79.4759
 ALT = 100
 RADIO = "Radio information goes here"
 ANTENNA = "Antenna information goes here"
 ENABLE_DEV = False #Do not touch for regular flights! Only for testing the SondeHub connetion!
-LAT_OFFSET = 0
-LONG_OFFSET = 0
-ALT_OFFSET = 0
+LAT_OFFSET = 0.5
+LONG_OFFSET = -9
+ALT_OFFSET = 20000
