5a4dcc9a8808fff766fae014ff9b0d65fd29154a
VE3SVF <VE3SVF@gmail.com>
2024-02-23 11:29:30 -0500

    Add some stuff

--- sondehub_config.py
+++ sondehub_config.py
@@ -1,10 +1,10 @@
-CALLSIGN = "TESTING"
+CALLSIGN = "Ground Station Name"
 LAT = 43.6689
 LONG = -79.4759
-ALT = 100
+ALT = 150
 RADIO = "Radio information goes here"
 ANTENNA = "Antenna information goes here"
 ENABLE_DEV = False #Do not touch for regular flights! Only for testing the SondeHub connetion!
-LAT_OFFSET = 0
-LONG_OFFSET = 0
-ALT_OFFSET = 0
+LAT_OFFSET = 0 #Do not change! Make sure this is zero!
+LONG_OFFSET = 0 #Do not change! Make sure this is zero!
+ALT_OFFSET = 0 #Do not change! Make sure this is zero!
